04da709823bee6cc78d7b6dcb201d1d39e44927c
Jash Nimje <jashnimje@gmail.com>
2021-07-05 16:27:14 +0530

    Search and Display

--- geocoder.js
+++ geocoder.js
@@ -2,7 +2,21 @@
 // Get an instance of the geocoding service:
 var geocoder = platform.getSearchService();
 
-function geocodeAndSearch() {
+function search() {
+    geocoder.geocode({
+        q: 'London'
+    }, (result) => {
+        // Add a marker for each location found
+        result.items.forEach((item) => {
+            console.log(item.position.lat, item.position.lng);
+            geocodeAndSearch(item.position.lat, item.position.lng);
+            map.addObject(new H.map.Marker(item.position));
+        });
+    }, alert);
+}
+search();
+
+function geocodeAndSearch(lat, lng) {
 
     let geocoderParams = {
         // q: 'Abbey',
@@ -27,9 +41,9 @@ function geocodeAndSearch() {
         // in: 'bbox:13.3817,52.50474,13.40432,52.51647' // Restrict parameter in a box shape
 
         // Search using Categories
-        // name: 'Electrify',
-        // at: '0.073998,51.484372',
-        // categories: '700-7600-0322'
+        name: 'EV Charging Station',
+        at: lat + "," + lng,
+        categories: '700-7600-0322'
 
         // name: 'museum',
         // at: '52.45722,13.38044',
@@ -43,28 +57,38 @@ function geocodeAndSearch() {
         // id: 'here:pds:place:276aabd1-9fd829e3684d05b86f7ddb1f3f83924c'
 
         // Search using reverse Geo code
-        at: '52.5415,13.39316'
+        // at: '52.5415,13.39316'
 
     }
 
     function onResult(result) {
         console.log(result);
+        result.items.forEach(item => {
+            let amps = item.extended.evStation.connectors[0].chargingPoint.ampsRange;
+            let noOfConnectors = item.extended.evStation.connectors[0].chargingPoint.numberOfConnectors;
 
-        //Plot Every Item on the map
-        // result.items.forEach(item => {
-        //     map.addObject(new H.map.Marker(item.position));
-        //     ui.addBubble(new H.ui.InfoBubble(item.position, {
-        //         content: 'Distance: ' + item.distance + ' m'
-        //     }));
-        // });
+            let pointerMarker = new H.map.Marker(item.position, {
+                icon: imageIcon,
+                volatility: true,
+            });
+            pointerMarker.setData("Location: " + item.position.lat + ", " + item.position.lng + " No. of Connectors: " + noOfConnectors + " Amps: " + amps);
+            map.addObject(pointerMarker);
+            if (item instanceof H.map.Marker) {
+                // var bubble = new H.ui.InfoBubble(item.getGeometry(), {
+                //     content: 'Distance: ' + item.distance + ' m' + amps, // data set while adding the marker object
+                // });
+                // show info bubble
+                ui.addBubble(bubble);
+            }
+        });
     }
 
     // geocoder.geocode(geocoderParams, onResult, alert);
     // geocoder.discover(geocoderParams, onResult, alert); // Discover Places nearby
     // geocoder.autosuggest(geocoderParams, onResult, alert); // Auto Complete incomplete suggestion
-    // geocoder.browse(geocoderParams, onResult, alert);
+    geocoder.browse(geocoderParams, onResult, alert);
     // geocoder.lookup(geocoderParams, onResult, alert);
-    geocoder.reverseGeocode(geocoderParams, onResult, alert);
+    // geocoder.reverseGeocode(geocoderParams, onResult, alert);
 }
 
-geocodeAndSearch();
+
